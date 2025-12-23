6623412dbc0c3d019c2e7250df203afe1dca5d18
florzoye <florzoye.official@gmail.com>
2025-12-23 14:17:14 +0700

    add llm_config

--- data/configs/redis_config.py
+++ data/configs/redis_config.py
@@ -1,4 +1,5 @@
 import os
+import ssl
 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
@@ -14,8 +15,6 @@ class RedisSettings(BaseSettings):
     REDIS_PASSWORD: str
     REDIS_HOST: str
     BASE_DIR: str = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    UPLOAD_DIR: str = os.path.join(BASE_DIR, 'app/uploads')
-    STATIC_DIR: str = os.path.join(BASE_DIR, 'app/static')
     model_config = SettingsConfigDict(
         env_file=os.path.join(BASE_DIR, ".env"),
         env_file_encoding="utf-8",
@@ -27,9 +26,20 @@ redis_url = f"redis://:{settings.REDIS_PASSWORD}@{settings.REDIS_HOST}:{settings
 
 celery_app = Celery("celery_worker", broker=redis_url, backend=redis_url)
 
+celery_app.conf.update(
+    task_serializer='json',
+    result_serializer='json',
+    accept_content=['json'],
+    enable_utc=True,
+    timezone='Europe/Moscow',
+    broker_connection_retry_on_startup=True,
+)
+
+
 redis_client = redis.Redis(
     host=settings.REDIS_HOST,
     port=settings.REDIS_PORT,
     db=0,
     password=settings.REDIS_PASSWORD
 )
+import src.app.queue.tasks
