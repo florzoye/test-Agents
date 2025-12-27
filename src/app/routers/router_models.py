ca030cc97f56f1f487381d82c29c8136ebf1f67e
florzoye <florzoye.official@gmail.com>
2025-12-28 00:32:31 +0700

    TelegramParseMode for sender

--- src/app/routers/router_models.py
+++ src/app/routers/router_models.py
@@ -1,19 +1,26 @@
 import aiohttp
+from enum import StrEnum
 from data.configs.tg_config import tg_settings
-
 from src.models.base.message_sender import MessageSender
+
+class TelegramParseMode(StrEnum):
+    HTML = "HTML"
+    MARKDOWN = "Markdown"
+    MARKDOWN_V2 = "MarkdownV2"
+    NONE = None  
 
 class TelegramSender(MessageSender):
     async def send_message(
-        self, 
+        self,
+        content: str | None = None,
         recipient_id: str | None = None, 
-        content: str | None = None
+        parse_mode: TelegramParseMode = TelegramParseMode.NONE,
     ) -> bool:
         tg_api_url = f"https://api.telegram.org/bot{tg_settings.BOT_TOKEN}/sendMessage"
         payload = {
             "chat_id": recipient_id,
             "text": content,
-            'parse_mode': 'HTML'
+            'parse_mode': parse_mode
         }
         async with aiohttp.ClientSession() as session:
             async with session.post(
