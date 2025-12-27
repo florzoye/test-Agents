41ac2bdcf1b3ba1ac62fe05d9561cff9d40b9fe7
florzoye <florzoye.official@gmail.com>
2025-12-28 00:01:46 +0700

    add StrEnum for Source class

--- src/models/messages.py
+++ src/models/messages.py
@@ -1,10 +1,14 @@
+from enum import StrEnum
 from datetime import datetime
 from pydantic import BaseModel, Field, ConfigDict
 
+class Source(StrEnum):
+    user = 'user'
+    agent = 'agent'
 
 class ClientMessage(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     
-    source: str  
+    source: Source  
     content: str
     timestamp: datetime = Field(default_factory=datetime.now)
