bdce4442875e189f600429268ab06cb93c7a5adf
florzoye <florzoye.official@gmail.com>
2025-12-27 23:45:46 +0700

    start class for ollama config

---
+++ data/configs/ollama_config.py
@@ -0,0 +1,16 @@
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+class OllamaConfig(BaseSettings):
+    OLLAMA_MODEL: str
+    
+    BASE_DIR: str = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(BASE_DIR, ".env"),
+        env_file_encoding="utf-8",
+    )
+
+ollama_config = OllamaConfig()
