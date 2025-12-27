835f9bd030e6dbbfd17cf0b4d79a5a4ad413fc73
florzoye <florzoye.official@gmail.com>
2025-12-27 23:49:11 +0700

    add inits files for data dir

---
+++ data/configs/__init__.py
@@ -0,0 +1,11 @@
+from .tg_config import tg_settings
+from .database_config import db_config
+from .llm_config import base_llm_config
+from .ollama_config import ollama_config
+from .gigachat_config import giga_chat_config
+from .redis_config import redis_client, celery_app
+
+__all__ = [
+    "tg_settings", "db_config", "base_llm_config", 
+    "ollama_config", "giga_chat_config", "redis_client", "celery_app"
+]
