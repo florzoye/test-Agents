ba2407747acc3281c3856a2cb70a53dd10e2f7d5
florzoye <florzoye.official@gmail.com>
2025-12-28 15:47:24 +0700

    add db type

--- data/configs/database_config.py
+++ data/configs/database_config.py
@@ -8,8 +8,10 @@ class DBConfig(BaseSettings):
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
-    DB_PASSWORD: str
     DB_NAME: str
+    DB_TYPE: str
+    DB_PASSWORD: str
+    SQLITE_PATH: str
     DB_DEBUG: bool = False
 
     BASE_DIR: str = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -26,4 +28,5 @@ class DBConfig(BaseSettings):
             f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
         )
 
+    
 db_config = DBConfig()
