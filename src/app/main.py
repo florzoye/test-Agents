b4b59949c3aeb49f042fc241a6e530f8c7758c9d
florzoye <florzoye.official@gmail.com>
2026-01-16 23:51:48 +0700

    add lifespan func for fastapi app

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,24 @@
+import asyncio
 from fastapi import FastAPI
+from chain import MultiAgentChain
 from src.app.routers import telegram
 
-app = FastAPI(title="Telegram Webhook")
+async def lifespan(app: FastAPI):
+    chain = MultiAgentChain()
+    graph = await chain.build_workflow()
 
+    app.state.agent_task = asyncio.create_task(
+        graph.ainvoke({})
+    )
+
+    print("🚀 Агент запущен")
+
+    yield  
+
+    task = getattr(app.state, "agent_task", None)
+    if task:
+        task.cancel()
+        print("🛑 Агент остановлен")
+
+app = FastAPI(title="Telegram Webhook", lifespan=lifespan)
 app.include_router(telegram.router)
-
