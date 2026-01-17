54149ef7dc6fd7ac425ddfff01eac205fc995a9f
florzoye <florzoye.official@gmail.com>
2026-01-17 22:32:54 +0700

    add parametrs for retry_async deco

--- src/core/agents/dialog_agent.py
+++ src/core/agents/dialog_agent.py
@@ -12,6 +12,7 @@ from src.core.agents.models.base import (
 from utils.decorators import retry_async
 from src.models.messages import BaseMessage
 from src.models.client_model import ClientModel
+from data.configs.base_config import base_config
 from src.core.agents.models.exc import AgentEnum
 from src.core.agents.prompts import DialogPromptTemplates
 from data.configs.callbacks_config import CALLBACK_SERVICE
@@ -29,17 +30,20 @@ class DialogAgent(BaseAgent):
     def __init__(
         self, 
         llm: BaseLLM, 
-        tools: list[BaseTool]
+        tools: list[BaseTool],
+        system_prompt: SystemMessage
     ):
         if self._initialized:
             return
         
         self._llm = llm
         self._tools = tools
+        self.system_prompt = system_prompt
         self.agent: Runnable | None = None
+
         self._ensure_agent()
         self._initialized = True
-
+    
     def _ensure_agent(self) -> None:
         try:
             if self.agent is None:
@@ -50,18 +54,21 @@ class DialogAgent(BaseAgent):
         except Exception as exp:
             raise AgentInitializationException(agent=AgentEnum.DIALOG, exp=exp)
 
-    @retry_async(attempts=3)
+    @retry_async(
+        attempts=base_config.ATTEMPS_FOR_RETRY,
+        backoff=base_config.BACKOFF,
+        delay=base_config.DELAY
+    )
     async def execute(
         self, 
         client_model: ClientModel, 
         user_message: BaseMessage, 
-        system_prompt: SystemMessage
     ) -> BaseMessage:
         try:
             messages = await DialogPromptTemplates.build_messages(
                 client=client_model,
                 message=user_message,
-                system_prompt=system_prompt
+                system_prompt=self.system_prompt
             )
             try:
                 result = await self.agent.ainvoke(
