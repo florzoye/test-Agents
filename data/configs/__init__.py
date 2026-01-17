8eeb4c7da093715c1140305c60d515de1b1bec3d
florzoye <florzoye.official@gmail.com>
2026-01-17 22:33:35 +0700

    add new base_config for future parametrs

--- data/configs/__init__.py
+++ data/configs/__init__.py
@@ -4,8 +4,11 @@ from .llm_config import base_llm_config
 from .ollama_config import ollama_config
 from .gigachat_config import giga_chat_config
 from .redis_config import redis_client, celery_app
+from .base_config import base_config
+from .callbacks_config import CALLBACK_SERVICE
 
 __all__ = [
     "tg_settings", "db_config", "base_llm_config", 
-    "ollama_config", "giga_chat_config", "redis_client", "celery_app"
+    "ollama_config", "giga_chat_config", "redis_client", "celery_app",
+    "CALLBACK_SERVICE", "base_config"
 ]
---
+++ data/configs/base_config.py
@@ -0,0 +1,18 @@
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+class BaseConfig(BaseSettings):
+    DELAY: float
+    BACKOFF: float
+    ATTEMPS_FOR_RETRY: int
+
+    BASE_DIR: str = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(BASE_DIR, ".env"),
+        env_file_encoding="utf-8",
+    )
+
+base_config = BaseConfig()
