d7f746c556e8588c410d9eb578d8ec8d0c5cf882
florzoye <florzoye.official@gmail.com>
2026-01-17 23:05:38 +0700

    add validate AIMessage

--- src/core/agents/dialog_agent.py
+++ src/core/agents/dialog_agent.py
@@ -1,13 +1,14 @@
+from typing import List
 from langchain.tools import BaseTool
-from langchain.messages import SystemMessage
+from langchain.messages import SystemMessage, AIMessage
 
 from data.configs.base_config import base_config
 from data.configs.callbacks_config import CALLBACK_SERVICE
 
 from utils.decorators import retry_async
-from src.models.messages import BaseMessage
 from src.models.client_model import ClientModel
 from src.core.agents.models.exc import AgentEnum
+from src.models.messages import BaseMessage, Source
 from src.core.agents.prompts import DialogPromptTemplates
 from src.core.agents.models.base import BaseLLM, BaseTool, Runnable, BaseAgent, CreateAgent 
 from src.core.agents.models.exc import AgentExecutionException, AgentInitializationException, LLMException
@@ -25,6 +26,7 @@ class DialogAgent(BaseAgent):
         self, 
         llm: BaseLLM, 
         tools: list[BaseTool],
+        *,
         system_prompt: SystemMessage
     ):
         if self._initialized:
@@ -65,12 +67,27 @@ class DialogAgent(BaseAgent):
                 system_prompt=self.system_prompt
             )
             try:
-                result = await self.agent.ainvoke(
+                result_raw: dict = await self.agent.ainvoke(
                     {"messages": messages},
                     config={"callbacks": CALLBACK_SERVICE.callbacks},
                 )
+
+                ai_messages: List[AIMessage] = [
+                    msg for msg in result_raw.get("messages", [])
+                    if isinstance(msg, AIMessage)
+                ]
+
+                content: str = "Контент не получен, пропусти данное сообщение"
+                if ai_messages:
+                    content = ai_messages[-1].content
+
+                return BaseMessage(
+                    content=content, 
+                    source=Source.agent, 
+                    tg_id=client_model.tg_id
+                )
+            
             except Exception as exp:
                 raise AgentExecutionException(agent=AgentEnum.DIALOG, exp=exp)
-            return result
         except Exception as exp:
             raise LLMException(agent=AgentEnum.DIALOG, exp=exp)
