84d687a0778836dbfcafd9ed0de5798035f43f69
florzoye <florzoye.official@gmail.com>
2026-01-17 22:15:44 +0700

    add hidden init and raies exc

--- src/core/agents/dialog_agent.py
+++ src/core/agents/dialog_agent.py
@@ -6,56 +6,70 @@ from src.core.agents.models.base import (
     BaseLLM, 
     BaseTool, 
     Runnable,
-    BaseAgent, 
+    BaseAgent,
+    CreateAgent 
 )
 from utils.decorators import retry_async
 from src.models.messages import BaseMessage
 from src.models.client_model import ClientModel
+from src.core.agents.models.exc import AgentEnum
 from src.core.agents.prompts import DialogPromptTemplates
 from data.configs.callbacks_config import CALLBACK_SERVICE
+from src.core.agents.models.exc import AgentExecutionException, AgentInitializationException, LLMException
 
-class DialogAgent:
+class DialogAgent(BaseAgent):
+    _instance = None
+    _initialized = False
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
     def __init__(
-        self,
-        llm: BaseLLM,
-        agent_factory: BaseAgent,
-        tools: list[BaseTool],
+        self, 
+        llm: BaseLLM, 
+        tools: list[BaseTool]
     ):
+        if self._initialized:
+            return
+        
         self._llm = llm
         self._tools = tools
-        self._agent_factory = agent_factory
         self.agent: Runnable | None = None
+        self._ensure_agent()
+        self._initialized = True
 
-    def init(self):
-        self.agent = self._agent_factory.lc_create_agent(
-            llm=self._llm,
-            tools=self._tools,
-        )
-        
+    def _ensure_agent(self) -> None:
+        try:
+            if self.agent is None:
+                self.agent = CreateAgent.lc_create_agent(
+                    llm=self._llm,
+                    tools=self._tools
+                )
+        except Exception as exp:
+            raise AgentInitializationException(agent=AgentEnum.DIALOG, exp=exp)
+
     @retry_async(attempts=3)
     async def execute(
-        self,
-        user_message: BaseMessage,
-        client_model: ClientModel,
+        self, 
+        client_model: ClientModel, 
+        user_message: BaseMessage, 
         system_prompt: SystemMessage
-    ):
-        if self.agent is None:
-            raise RuntimeError("Agent not initialized, init() first!")
-
+    ) -> BaseMessage:
         try:
             messages = await DialogPromptTemplates.build_messages(
                 client=client_model,
                 message=user_message,
                 system_prompt=system_prompt
             )
-
-            result = await self.agent.ainvoke(
-                {"messages": messages},
-                config={"callbacks": CALLBACK_SERVICE.callbacks},
-            )
-
+            try:
+                result = await self.agent.ainvoke(
+                    {"messages": messages},
+                    config={"callbacks": CALLBACK_SERVICE.callbacks},
+                )
+            except Exception as exp:
+                raise AgentExecutionException(agent=AgentEnum.DIALOG, exp=exp)
             return result
-
-        except Exception as e:
-            logger.exception(e)
-            return "Сервис временно недоступен"
+        except Exception as exp:
+            raise LLMException(agent=AgentEnum.DIALOG, exp=exp)
