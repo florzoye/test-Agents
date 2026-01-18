4ca846fa0727555aba69b00b819f83378d544d56
florzoye <florzoye.official@gmail.com>
2026-01-18 20:12:17 +0700

    add new custom research agent

--- src/core/agents/agent_singleton.py
+++ src/core/agents/agent_singleton.py
@@ -4,7 +4,7 @@ from langchain.messages import AnyMessage
 
 from src.core.agents.tools.base_tools import dialog_tools
 from src.core.agents.models.base import BaseAgentSingleton, BaseLLM
-from src.core.agents.prompts import DialogPromptTemplates, SummaryPromptTemplates
+from src.core.agents.prompts import DialogPromptTemplates, ResearchPromptTemplates
 from src.core.agents.models.exc import AgentEnum, AgentInitializationException, LLMException
 
 class DialogAgent(BaseAgentSingleton):
@@ -24,18 +24,18 @@ class DialogAgent(BaseAgentSingleton):
             system_prompt=self.system_prompt
         )
 
-class SummaryAgent(BaseAgentSingleton):
+class ResearchAgent(BaseAgentSingleton):
     def __init__(self, llm: BaseLLM, tools: Optional[List[BaseTool]] = None, system_prompt=None):
         super().__init__(llm, tools=tools or [], system_prompt=system_prompt)
 
     def _get_init_exception(self, exp: Exception) -> Exception:
-        return AgentInitializationException(agent=AgentEnum.SUMMARY, exp=exp)
+        return AgentInitializationException(agent=AgentEnum.RESEARCH, exp=exp)
 
     def _get_execute_exception(self, exp: Exception) -> Exception:
-        return LLMException(agent=AgentEnum.SUMMARY, exp=exp, message="Ошибка при выполнении execute")
+        return LLMException(agent=AgentEnum.RESEARCH, exp=exp, message="Ошибка при выполнении execute")
 
     async def _build_messages(self, client_model, user_message) -> list[AnyMessage]:
-        return await SummaryPromptTemplates.build_message(
+        return await ResearchPromptTemplates.build_message(
             client=client_model,
             message=user_message,
             system_prompt=self.system_prompt
