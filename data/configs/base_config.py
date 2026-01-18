3f9478e640fdf038cd541e6104b5689832a0991f
florzoye <florzoye.official@gmail.com>
2026-01-18 16:28:58 +0700

    add new MAX_CONCURRENT_EXECUTE  param

--- data/configs/base_config.py
+++ data/configs/base_config.py
@@ -8,6 +8,7 @@ class BaseConfig(BaseSettings):
     DELAY: float
     BACKOFF: float
     ATTEMPS_FOR_RETRY: int
+    MAX_CONCURRENT_EXECUTE: int
 
     BASE_DIR: str = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     model_config = SettingsConfigDict(
