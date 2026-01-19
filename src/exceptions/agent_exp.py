9adabe79bb898fa3eba389f5c9b63b8d2e863f45
florzoye <florzoye.official@gmail.com>
2026-01-19 22:37:58 +0700

    add seperate path for exc

---
+++ src/exceptions/agent_exp.py
@@ -0,0 +1,23 @@
+from src.enum.exc import AgentEnum
+
+class AgentException(Exception):
+    def __init__(self, message: str, agent: AgentEnum, exp: Exception):
+        self.exc = exp
+        self.agent = agent
+        self.message = message
+        super().__init__(f"[{agent}] {message} - {exp}")
+    
+    def __str__(self):
+        return f"[{self.agent}] {self.message} - {self.exc}"
+
+class AgentInitializationException(AgentException):
+    def __init__(self, agent: AgentEnum, exp: Exception, message: str = "Ошибка при инициализации агента", ):
+        super().__init__(message=message, agent=agent,exp=exp)
+
+class AgentExecutionException(AgentException):
+    def __init__(self, agent: AgentEnum, exp: Exception, message: str = "Ошибка при выполнении execute"):
+        super().__init__(message=message, agent=agent,exp=exp)
+
+class LLMException(AgentException):
+    def __init__(self, agent: AgentEnum, exp: Exception, message: str):
+        super().__init__(message=message, agent=agent,exp=exp)
