6ea172e955e5fd4ca8b6b33a4e2ba215de7d04e5
florzoye <florzoye.official@gmail.com>
2026-01-20 00:03:22 +0700

    delete files

--- src/utils/llm_adapter.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from langchain_core.language_models import BaseChatModel
-
-class LLMAdapter(ABC):
-    @abstractmethod
-    async def build(self, llm: BaseChatModel) -> BaseChatModel:
-        ... 
