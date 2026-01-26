e878d849064043bf3bf70a81dde2de1067e3c884
florzoye <florzoye.official@gmail.com>
2026-01-26 15:14:27 +0700

    integrated response_format

--- src/factories/agent_factory.py
+++ src/factories/agent_factory.py
@@ -1,24 +1,16 @@
-from typing import TYPE_CHECKING
 from langchain.agents import create_agent
 from langchain.messages import SystemMessage
 from langchain_core.runnables import Runnable
 
-if TYPE_CHECKING:
-    from src.core.agents.models.base import BaseLLM
-
 class AgentFactory:
-    async def lc_create_agent(
-        self,
-        llm: "BaseLLM",
-        system_prompt: SystemMessage
-    ) -> Runnable:
-        from data.init_configs import MIDDLEWARE_SERVICE
-        
-        # llm_instance = await llm.get_llm()
+    async def lc_create_agent(self, llm, system_prompt: SystemMessage) -> Runnable:
+        from data.init_configs import get_config
+        cfg = get_config()
         agent = create_agent(
             model=llm,
             system_prompt=system_prompt,
-            middleware=MIDDLEWARE_SERVICE.middlewares,
-        ) 
+            middleware=cfg.MIDDLEWARE_SERVICE.middlewares,
+            response_format=getattr(llm, "_response_format", None),
+        )
 
         return agent
